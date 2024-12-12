ca9dee902db0057c9979e5a2679d60597eba4151
ediw8311xht <maxballard333@gmail.com>
2024-12-11 19:15:15 -0500

    Z

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Disabling annoying shit because i wanna glob babyyyyyyyyyyyyyyyy
+# using $pargs gotta glob unless using xargs don't feel like it iagjsdijfiasjfiasidfj
+# posix compliant waht???????
+# shellcheck disable=SC2086
+
 _check_args() {
     [ "$#" -le 1      ] && return 1
     [ "${1}" = "${2}" ] && return 0
@@ -8,18 +13,19 @@ _check_args() {
     _check_args "${_c}" "${@}"
 }
 _run_this() {
+    _pargs='--docroot / --deploy-path / --http-address 0.0.0.0  --http-port 9090'
     binary="main"
     if _check_args '-c' "${@}" ; then
         make clean
-    elif make -j8 2>log.txt ; then
-        ./"${binary}"               \
-            --docroot /             \
-            --deploy-path /         \
-            --http-address 0.0.0.0  \
-            --http-port 9090
+    elif _check_args '-v' "${@}" ; then
+        valgrind ./"${binary}" $_pargs
     else
-        echo "Error....."
-        _check_args '-l' "${@}" && ${PAGER} 'log.txt'
+        if make -j8 2>log.txt ; then
+            ./"${binary}" $_pargs
+        else
+            echo "Error....."
+            _check_args '-l' "${@}" && ${PAGER} 'log.txt'
+        fi
     fi
 }
 
